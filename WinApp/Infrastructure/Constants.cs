45ae8f702e50dea788f6a4efe597e5c8b7e6c0f8
Toliman <skylake525@gmail.com>
2024-11-08 22:13:48 +0200

    minor change

--- WinApp/Infrastructure/Constants.cs
+++ WinApp/Infrastructure/Constants.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WinApp.Infrastructure
+﻿namespace WinApp.Infrastructure
 {
-    public static class Constants
+	public static class Constants
     {
         public static string DefaultDbConnection = "default";
         public static string EmptyInput = "Please, enter the {0}.";
         public static string InputTextBoxRegex = @"\b[\p{L}\p{M}'-]+\b";
         public static string DefaultShowNextButtonText = "I remember!";
-        public static string ChangedShowNextButtonText = "Show the next word";
+        public static string ChangedShowNextButtonText = "Next word";
     }
 }
