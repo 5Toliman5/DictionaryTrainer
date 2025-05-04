7f22af5396739a832b41fbb0d79e62852c78b3e5
Toliman <skylake525@gmail.com>
2025-05-04 17:42:40 +0300

    program.cs refactoring

---
+++ WinApp/DependencyResolver.cs
@@ -0,0 +1,30 @@
+﻿using DictinaryTrainer.BusinessLogic.Services.Abstract;
+using DictinaryTrainer.BusinessLogic.Services;
+using DictionaryTrainer.DAL.Repositories.Abstract;
+using DictionaryTrainer.WinApp.Presenter;
+using Microsoft.Extensions.DependencyInjection;
+using WinApp;
+using System.Configuration;
+using DictionaryTrainer.DAL.Repositories;
+
+namespace DictionaryTrainer.WinApp
+{
+	internal class DependencyResolver
+	{
+		public static IServiceCollection ConstructServices()
+		{
+			var services = new ServiceCollection();
+			services.AddSingleton<MainForm>();
+
+			var connString = ConfigurationManager.AppSettings["ConnectionString"];
+			services.AddScoped<IUserRepository>(x => new UserRepository(connString));
+			services.AddScoped<IWordRepository>(x => new WordRepository(connString));
+
+			services.AddScoped<IUserService, UserService>();
+			services.AddScoped<IWordTrainerService, WordTrainerService>();
+			services.AddScoped<IMainFormPresenter, MainFormPresenter>();
+
+			return services;
+		}
+	}
+}
--- WinApp/Program.cs
+++ WinApp/Program.cs
@@ -1,41 +1,22 @@
-using DictinaryTrainer.BusinessLogic.Services;
-using DictinaryTrainer.BusinessLogic.Services.Abstract;
-using DictionaryTrainer.DAL.Repositories;
-using DictionaryTrainer.DAL.Repositories.Abstract;
-using DictionaryTrainer.WinApp.Presenter;
+using DictionaryTrainer.WinApp;
 using Microsoft.Extensions.DependencyInjection;
-using System.Configuration;
-using WinApp;
 
 namespace WinApp
 {
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.SetCompatibleTextRenderingDefault(false);
-            ApplicationConfiguration.Initialize();
-            var services = new ServiceCollection();
-            ConfigureServices(services);
-            using var serviceProvider = services.BuildServiceProvider();
-            var mainForm = serviceProvider.GetRequiredService<MainForm>();
-            Application.Run(mainForm);
-        }
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton<MainForm>();
-
-            var connString = ConfigurationManager.AppSettings["ConnectionString"];
-			services.AddScoped<IUserRepository>(x => new UserRepository(connString));
-			services.AddScoped<IWordRepository>(x => new WordRepository(connString));
-
-			services.AddScoped<IUserService, UserService>();
-			services.AddScoped<IWordTrainerService, WordTrainerService>();
-			services.AddScoped<IMainFormPresenter, MainFormPresenter>();
+	internal static class Program
+	{
+		/// <summary>
+		///  The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.SetCompatibleTextRenderingDefault(false);
+			ApplicationConfiguration.Initialize();
+			var services = DependencyResolver.ConstructServices();
+			using var serviceProvider = services.BuildServiceProvider();
+			var mainForm = serviceProvider.GetRequiredService<MainForm>();
+			Application.Run(mainForm);
 		}
-    }
+	}
 }
